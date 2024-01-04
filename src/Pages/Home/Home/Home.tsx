4adaba533a28ec492182503160c6a481e9edaf1b
hasibferdous <hasibferdous.ict@gmail.com>
2024-01-04 14:23:29 +0600

    footer section done

--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -2,6 +2,9 @@ const Home = () => {
     return (
         <div>
             <h1>This is Home</h1>
+            <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
         </div>
     );
 };
